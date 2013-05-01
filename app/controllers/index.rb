2d9ae28baf3894d5f2a35d5273223dc9df520d7c
stephenitis <nguyenhstephen@gmail.com>
2013-05-01 00:42:50 -0700

    changed css styling for basic gallery

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,5 @@
+require 'tempfile'
+
 get '/' do
   # Look in app/views/index.erb
   erb :upload
@@ -8,17 +10,28 @@ get '/upload' do
 end
 
 
+# post '/upload' do
+#   p params
+#   # p file1 = params[:file][:tempfile][0]
+#   # p "tempfile2 "
+#   # p file2 = params[:file][:tempfile][1]
+#   # File.open(File.join(Dir.pwd,"public/uploads", "hello"), "wb") { |f| f.write(file1.read) }
+#   # puts 'success'
+#   # File.open(File.join(Dir.pwd,"public/uploads", "world"), "wb") { |f| f.write(file2.read) }
+#   #  puts 'success'
+# end
+
+
 post '/upload' do
+  "hello"
   filename = params[:file][:filename]
   tempfile = params[:file][:tempfile]
   @target = "public/uploads/#{filename}"
   Photo.create()
   File.open(@target, 'wb') {|f| f.write tempfile.read }
   @filepath = "/uploads/#{filename}"
-  p @filepath
   a_photo = Photo.create(filepath: @filepath)
-  p a_photo
-  erb :gallery
+  redirect '/gallery'
 end
 
 get '/show' do
@@ -31,7 +44,6 @@ content_type:json
 @gallery = Photo.all.map {|x| x.filepath}
 p @gallery.to_json
 end
-
 
 get '/gallery' do
   erb :gallery
